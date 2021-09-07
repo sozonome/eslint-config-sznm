99ac8edbf8ab2671a529a43c49c2fb94c7cc69fc
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-09-07 19:56:49 +0700

    feat(react): add rules

--- react/index.js
+++ react/index.js
@@ -1,4 +1,4 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['./extensions.js', '../bare.js'],
+  extends: ['./extensions.js', '../bare.js', './rules.js'],
 };
---
+++ react/rules.js
@@ -0,0 +1,7 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'react/require-default-props': 'off',
+  },
+};
